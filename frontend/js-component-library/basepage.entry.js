5231407b8071cac1503bf531b4bc496ee31e0ecf
paulAlexSerban <paul.alex.serban@gmail.com>
2022-10-20 11:52:21 +0300

    feat: initia file structure

---
+++ frontend/js-component-library/basepage.entry.js
@@ -0,0 +1,11 @@
+import './basepage.styles.scss';
+
+// import JS Components
+import Header from '../../ui_ux/layout/header/header';
+import Story from '../../ui_ux/layout/story/story';
+
+/**
+ * Example
+ * import headerStructure from '../../ui_ux/layout/header/header';
+ * new headerStructure;
+ */
